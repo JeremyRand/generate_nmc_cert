1b38757a3e6d2fc6fb9b485515185b878cfe3c25
Jeremy Rand <jeremyrand@danwin1210.de>
2025-09-03 11:46:57 +0000

    Undo documentation wording change
    I failed to notice that "default" was already visible in the usage help.

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var (
 	//isCA       = flag.Bool("ca", false, "whether this cert should be its own Certificate Authority")
 	//rsaBits    = flag.Int("rsa-bits", 2048, "Size of RSA key to generate. Ignored if --ecdsa-curve is set")
 	//ecdsaCurve = flag.String("ecdsa-curve", "", "ECDSA curve to use to generate a key. Valid values are P224, P256 (recommended), P384, P521")
-	ecdsaCurve = flag.String("ecdsa-curve", "P256", "ECDSA curve to use to generate a key. Valid values are P224, P256 (default), P384, P521")
+	ecdsaCurve = flag.String("ecdsa-curve", "P256", "ECDSA curve to use to generate a key. Valid values are P224, P256 (recommended), P384, P521")
 	ed25519Key = flag.Bool("ed25519", false, "Generate an Ed25519 key")
 	parentKey  = flag.String("parent-key", "", "(Optional) Path to existing CA private key to sign end-entity cert with")
 	parentChain = flag.String("parent-chain", "", "(Optional) Path to existing CA cert chain to sign end-entity cert with")
